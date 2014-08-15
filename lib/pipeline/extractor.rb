88c38a75b33fcdb1d9c9691d28b8044e58d23c2f
Philip Stevens <philip.stevens@reevoo.com>
2014-08-15 16:32:54 +0100

    Begin to document and construct the expected appearance for the extractor and transformers.

--- lib/pipeline/extractor.rb
+++ lib/pipeline/extractor.rb
@@ -1,4 +1,10 @@
 module Pipeline
+  # Include this module in a class to register it as an extractor.
+  #
+  # A connection is expected to contain a method called `exec` that can receive
+  # a `command` parameter and yield a result.
+  #
+  # A command is expected to be a query of some form that can be run on the `connection`.
   module Extractor
     def self.included(base)
       base.extend(ClassMethods)
@@ -6,6 +12,13 @@ module Pipeline
     end
 
     module ClassMethods
+      def connection(connection)
+        @connection = connection
+      end
+
+      def command(command)
+        @command = command
+      end
     end
   end
 end
--- lib/pipeline/transformer.rb
+++ lib/pipeline/transformer.rb
@@ -1,4 +1,8 @@
 module Pipeline
+  # Include this module in a class to register it as an transformer.
+  # Transformers are ordered by the loading order, or as specified by
+  # setting the position value to a non-negative integer; where 1 has the
+  # highest priority
   module Transformer
     def self.included(base)
       base.extend(ClassMethods)
@@ -6,6 +10,12 @@ module Pipeline
     end
 
     module ClassMethods
+      def position(value)
+        value = value.to_i
+        if value > 0
+          @position = value
+        end
+      end
     end
   end
 end
