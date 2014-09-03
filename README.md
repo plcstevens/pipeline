85dc5be64d9f6b0da78c8cdcc462336642f82a4a
Philip Stevens <philip.stevens@reevoo.com>
2014-09-03 13:47:36 +0100

    Updated read me.

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ And then execute:
 
 Or install it yourself as:
 
-    $ gem install pipeline
+    $ gem install reevoo-pipeline
 
 ## Usage
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,12 @@
 require 'simplecov'
+SimpleCov.start { add_filter '/spec/' }
+
 require 'pry'
-
-SimpleCov.start do
-  add_filter '/spec/'
-  add_filter '/.bundle/'
-end
-
 require 'pipeline'
 require 'logger'
 
-Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
-
+Dir['./spec/support/**/*.rb'].each { |f| require f }
 RSpec.configure do |config|
-  # Set log level to the highest
-  config.before(:all) do
-    Pipeline::Config.log_level = Logger::FATAL
-  end
-
-  # Raise errors on any deprecation warnings.
+  config.before(:all) { Pipeline::Config.log_level = Logger::FATAL }
   config.raise_errors_for_deprecations!
 end
